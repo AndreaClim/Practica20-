a7f8cd59e99e376bef703c207912cea268e74bee
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-02-14 17:56:55 -0600

    Libros con sus campos

--- Practica/Models/libros.cs
+++ Practica/Models/libros.cs
@@ -1,6 +1,15 @@
-﻿namespace Practica.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica.Models
 {
     public class libros
     {
+        [Key]
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public int anioPublicacion { get; set; }
+        public int autorId { get; set; }
+        public int categoriaId { get; set; }
+        public string resumen {  get; set; }
     }
 }
