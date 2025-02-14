0d3d71748d96ebbe47f5faf3117716edc44caaff
miche <miche@DESKTOP-EEADCHD>
2025-02-14 17:54:22 -0600

    commit 5

--- Practica/Program.cs
+++ Practica/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Practica.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<autoresContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("autoresContext")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
