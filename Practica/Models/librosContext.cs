e7759048b6f0be7f67a572abcd307cc83db4cbbf
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-02-14 17:43:12 -0600

    Tito se la come

---
+++ Practica/Models/librosContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Practica.Models
+{
+    public class librosContext : DbContext  
+    {
+        public librosContext(DbContextOptions<librosContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
