645d516ab4c3d7c9cea11bb41840aedc48dbc6c5
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-02-14 18:22:42 -0600

    Controlador

---
+++ Practica/Controllers/librosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class librosController : ControllerBase
+    {
+        private readonly librosContext _librosContexto;
+
+        public librosController(librosContext librosContexto) 
+        {
+            _librosContexto = librosContexto;
+        }
+
+        ///<summary>
+        ///Endpoint que devuelve todos los libros 
+        ///</summary>
+        ///<returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get() 
+        {
+            List<libros> listadoLibros = (from e in _librosContexto.libros
+                                          select e).ToList();
+
+            if (listadoLibros.Count == 0) 
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoLibros);
+        }
+
+    }
+}
--- Practica/Models/autoresContext.cs
+++ Practica/Models/autoresContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;  
+
 namespace Practica.Models
 {
     public class autoresContext : DbContext
