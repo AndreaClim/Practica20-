992accc0d5826ce46cb7c696e15f4f106415d680
miche <miche@DESKTOP-EEADCHD>
2025-02-14 18:03:17 -0600

    commit 6

--- Practica/Models/autores.cs
+++ Practica/Models/autores.cs
@@ -1,6 +1,12 @@
-﻿namespace Practica.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Practica.Models
 {
     public class autores
     {
+        [Key]
+        public int autorId { get; set; }
+        public string nombre { get; set; }
+        public string nacionalidad { get; set; }
     }
 }
+
